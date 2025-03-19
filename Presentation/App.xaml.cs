cc1bd8aac1a6fe827859aa070d1e303e42c02879
jeyjunior <jeyjuniordg@gmail.com>
2025-03-19 07:03:35 -0300

    Removido todo padrao de design anterior, add pacotes do MaterialDesign

--- Presentation/App.xaml.cs
+++ Presentation/App.xaml.cs
@@ -1,6 +1,5 @@
-﻿using Application;
-using System;
-using System.Runtime.InteropServices;
+﻿using System.Configuration;
+using System.Data;
 using System.Windows;
 
 namespace Presentation
@@ -10,13 +9,6 @@ namespace Presentation
     /// </summary>
     public partial class App : System.Windows.Application
     {
-        private void Application_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
-        {
-        }
-
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            Bootstrap.Inicializar();
-        }
     }
+
 }
